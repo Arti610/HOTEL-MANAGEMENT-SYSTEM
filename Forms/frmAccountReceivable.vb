682e4722f79291a34be1b978f3cacb1d7521bf8d
Arti <artinirmalkar06@gmail.com>
2022-07-02 10:34:21 +0530

    complete Hotel Management System using .net framework

---
+++ Forms/frmAccountReceivable.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmAccountReceivable
+
+    Private Sub cmdSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSave.Click
+
+    End Sub
+End Class
---
+++ Modules/modPublicVar.vb
@@ -0,0 +1,10 @@
+﻿'---------------------------------------
+'Programmed by: Jomar I. Pabuaya
+'Website: http://visualbasicproject.blogspot.com
+'---------------------------------------
+
+Module modPublicVar
+    Public CurrUser As USER_INFO
+    Public CurrBiz As BUSINESS_INFO
+    Public CurrCust As Customer
+End Module
